234346f82269cd018160d608d3f92bc8e45cad60
Dennis Iversen <dennis.iversen@gmail.com>
2017-06-16 21:32:40 +0200

    auto commit

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var nF = require ('./index.js');
 // if you have installed module with npm 
 // You should:
 
-// var nF = require('note-frequency');
+// var nF = require('tone-frequency');
 console.log("Base tone = " +nF.base );
 console.log('Steps = ' + nF.numSteps);
 console.log('C from base A = ' + nF.getToneStep(3));
