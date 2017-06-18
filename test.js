00f10c9bc0700da99ff77b3c600d0547d5a6f234
Dennis Iversen <dennis.iversen@gmail.com>
2017-06-18 12:46:22 +0200

    auto commit

--- test.js
+++ test.js
@@ -1,15 +1,14 @@
 var nF = require ('./index.js');
 
-// if you have installed module with npm 
-// You should:
+// if you have installed module with npm you should:
 // var nF = require('tone-frequency');
 nF.numSteps = 12;
 nF.base = 440;
 
-console.log("Base tone = " +nF.base );
-console.log('Steps = ' + nF.numSteps);
-console.log('A = ' + nF.getToneStep(0));
-console.log('D = ' + nF.getToneStep(5));
-console.log('A (an octave lower) = ' + nF.getToneStep(-12));
-console.log('A an octave higher = ' + nF.getToneStep(12));
+console.log("Base tone = " +nF.base ); // => 440
+console.log('Steps = ' + nF.numSteps); // => 12
+console.log('A = ' + nF.getToneStep(0)); // 440
+console.log('D = ' + nF.getToneStep(5)); // 587.3295358348151
+console.log('A (an octave lower) = ' + nF.getToneStep(-12)); // => 220
+console.log('A an octave higher = ' + nF.getToneStep(12)); // => 880
 
